0085e8bca1c5e7a0294f14fb785093f0e53ec40a
Akın Ayturan <akinayturan@gmail.com>
2015-02-03 16:53:47 +0200

    6 Band Resistor Color Code Calculator

---
+++ 6-band-resistor-color-code-calculator/20893427_Yasin_Akin_Ayturan_Odev_2_C_Sharp/Form1.Designer.cs
@@ -0,0 +1,375 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Siyah",
+            "Kahverengi",
+            "Kırmızı",
+            "Turuncu",
+            "Sarı",
+            "Yeşil",
+            "Mavi",
+            "Mor",
+            "Gri",
+            "Beyaz"});
+            this.comboBox1.Location = new System.Drawing.Point(6, 42);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(115, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Siyah",
+            "Kahverengi",
+            "Kırmızı",
+            "Turuncu",
+            "Sarı",
+            "Yeşil",
+            "Mavi",
+            "Mor",
+            "Gri",
+            "Beyaz"});
+            this.comboBox2.Location = new System.Drawing.Point(127, 42);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(115, 21);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Yok",
+            "Siyah",
+            "Kahverengi",
+            "Kırmızı",
+            "Turuncu",
+            "Sarı",
+            "Yeşil",
+            "Mavi",
+            "Mor",
+            "Gri",
+            "Beyaz"});
+            this.comboBox3.Location = new System.Drawing.Point(248, 42);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(115, 21);
+            this.comboBox3.TabIndex = 2;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "Siyah",
+            "Kahverengi",
+            "Kırmızı",
+            "Turuncu",
+            "Sarı",
+            "Yeşil",
+            "Mavi",
+            "Mor",
+            "Altın",
+            "Gümüş"});
+            this.comboBox4.Location = new System.Drawing.Point(31, 128);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(115, 21);
+            this.comboBox4.TabIndex = 3;
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Items.AddRange(new object[] {
+            "Yok",
+            "Kahverengi",
+            "Kırmızı",
+            "Yeşil",
+            "Mavi",
+            "Mor",
+            "Altın",
+            "Gümüş"});
+            this.comboBox5.Location = new System.Drawing.Point(152, 128);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(115, 21);
+            this.comboBox5.TabIndex = 4;
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Items.AddRange(new object[] {
+            "Yok",
+            "Kahverengi",
+            "Kırmızı",
+            "Turuncu",
+            "Sarı"});
+            this.comboBox6.Location = new System.Drawing.Point(273, 128);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(115, 21);
+            this.comboBox6.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox1.Location = new System.Drawing.Point(132, 223);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(262, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.comboBox3);
+            this.groupBox1.Location = new System.Drawing.Point(25, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(385, 84);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Renk kodları";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(245, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "3.band";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(124, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "2.band";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(6, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "1.band";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(31, 226);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Direnç Değeri:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(34, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Çarpan";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(149, 103);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Tolerans";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(270, 103);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(79, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Sıcaklık Kat.";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(36, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(33, 17);
+            this.radioButton1.TabIndex = 10;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Ω";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(154, 19);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(39, 17);
+            this.radioButton2.TabIndex = 11;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "kΩ";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(276, 19);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(42, 17);
+            this.radioButton3.TabIndex = 12;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "MΩ";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Controls.Add(this.radioButton3);
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Location = new System.Drawing.Point(31, 166);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(363, 47);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ohm Cinsi";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(109, 253);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(229, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "20893427 / Yasin Akın Ayturan / Ödev 2 / C #";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 275);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox6);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.comboBox4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Direnç Hesaplayıcı";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox comboBox6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ 6-band-resistor-color-code-calculator/20893427_Yasin_Akin_Ayturan_Odev_2_C_Sharp/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Text = "Siyah";
+            comboBox2.Text = "Siyah";
+            comboBox3.Text = "Yok";
+            comboBox4.Text = "Siyah";
+            comboBox5.Text = "Yok";
+            comboBox6.Text = "Yok";
+
+            comboBox1.SelectedIndexChanged += new EventHandler(comboBox4_SelectedIndexChanged);
+            comboBox2.SelectedIndexChanged += new EventHandler(comboBox4_SelectedIndexChanged);
+            comboBox3.SelectedIndexChanged += new EventHandler(comboBox4_SelectedIndexChanged);
+            comboBox5.SelectedIndexChanged += new EventHandler(comboBox4_SelectedIndexChanged);
+            comboBox6.SelectedIndexChanged += new EventHandler(comboBox4_SelectedIndexChanged);
+            radioButton1.CheckedChanged += new EventHandler(comboBox4_SelectedIndexChanged);
+            radioButton2.CheckedChanged += new EventHandler(comboBox4_SelectedIndexChanged);
+            radioButton3.CheckedChanged += new EventHandler(comboBox4_SelectedIndexChanged);
+            radioButton1.Checked = true;
+            
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string carpan_1 = "", carpan_2 = "", carpan_3 = "0", sck="Yok", tol="Yok";
+            double toplam = 0, direnc_degeri = 0, carpan = 0;
+
+            if (comboBox1.Text == "Siyah") {carpan_1 = "0"; label1.ForeColor=Color.Black;}
+            else if (comboBox1.Text == "Kahverengi") { carpan_1 = "1"; label1.ForeColor = Color.Brown; }
+            else if (comboBox1.Text == "Kırmızı") { carpan_1 = "2"; label1.ForeColor = Color.Red; }
+            else if (comboBox1.Text == "Turuncu") {carpan_1 = "3"; label1.ForeColor = Color.Orange;}
+            else if (comboBox1.Text == "Sarı") {carpan_1 = "4"; label1.ForeColor = Color.Yellow;}
+            else if (comboBox1.Text == "Yeşil") {carpan_1 = "5"; label1.ForeColor = Color.Green;}
+            else if (comboBox1.Text == "Mavi") {carpan_1 = "6"; label1.ForeColor = Color.Blue;}
+            else if (comboBox1.Text == "Mor") {carpan_1 = "7"; label1.ForeColor = Color.Purple;}
+            else if (comboBox1.Text == "Gri") {carpan_1 = "8"; label1.ForeColor = Color.Gray;}
+            else if (comboBox1.Text == "Beyaz") { carpan_1 = "9"; label1.ForeColor = Color.White; }
+
+            if (comboBox2.Text == "Siyah") { carpan_2 = "0"; label2.ForeColor = Color.Black; }
+            else if (comboBox2.Text == "Kahverengi") {carpan_2 = "1"; label2.ForeColor = Color.Brown; }
+            else if (comboBox2.Text == "Kırmızı") {carpan_2 = "2"; label2.ForeColor = Color.Red; }
+            else if (comboBox2.Text == "Turuncu") {carpan_2 = "3"; label2.ForeColor = Color.Orange;}
+            else if (comboBox2.Text == "Sarı") {carpan_2 = "4"; label2.ForeColor = Color.Yellow;}
+            else if (comboBox2.Text == "Yeşil") {carpan_2 = "5"; label2.ForeColor = Color.Green;}
+            else if (comboBox2.Text == "Mavi") {carpan_2 = "6"; label2.ForeColor = Color.Blue;}
+            else if (comboBox2.Text == "Mor") {carpan_2 = "7"; label2.ForeColor = Color.Purple;}
+            else if (comboBox2.Text == "Gri") {carpan_2 = "8"; label2.ForeColor = Color.Gray;}
+            else if (comboBox2.Text == "Beyaz") {carpan_2 = "9"; label2.ForeColor = Color.White; }
+
+            if (comboBox3.Text == "Siyah") { carpan_3 = "0"; label3.ForeColor = Color.Black; }
+            else if (comboBox3.Text == "Kahverengi") {carpan_3 = "1"; label3.ForeColor = Color.Brown; }
+            else if (comboBox3.Text == "Kırmızı") {carpan_3 = "2"; label3.ForeColor = Color.Red; }
+            else if (comboBox3.Text == "Turuncu") {carpan_3 = "3"; label3.ForeColor = Color.Orange;}
+            else if (comboBox3.Text == "Sarı") {carpan_3 = "4"; label3.ForeColor = Color.Yellow;}
+            else if (comboBox3.Text == "Yeşil") {carpan_3 = "5"; label3.ForeColor = Color.Green;}
+            else if (comboBox3.Text == "Mavi") {carpan_3 = "6"; label3.ForeColor = Color.Blue;}
+            else if (comboBox3.Text == "Mor") {carpan_3 = "7"; label3.ForeColor = Color.Purple;}
+            else if (comboBox3.Text == "Gri") {carpan_3 = "8"; label3.ForeColor = Color.Gray;}
+            else if (comboBox3.Text == "Beyaz") {carpan_3 = "9"; label3.ForeColor = Color.White; }
+            
+            if (comboBox4.Text == "Siyah") carpan = (int)Math.Pow(10, 0);
+            else if (comboBox4.Text == "Kahverengi") carpan = (int)Math.Pow(10, 1);
+            else if (comboBox4.Text == "Kırmızı") carpan = (int)Math.Pow(10, 2);
+            else if (comboBox4.Text == "Turuncu") carpan = (int)Math.Pow(10, 3);
+            else if (comboBox4.Text == "Sarı") carpan = (int)Math.Pow(10, 4);
+            else if (comboBox4.Text == "Yeşil") carpan = (int)Math.Pow(10, 5);
+            else if (comboBox4.Text == "Mavi") carpan = (int)Math.Pow(10, 6);
+            else if (comboBox4.Text == "Mor") carpan = (int)Math.Pow(10, 7);
+            else if (comboBox4.Text == "Altın") carpan = (int)Math.Pow(10, 8);
+            else if (comboBox4.Text == "Gümüş") carpan = (int)Math.Pow(10, 9);
+           
+            if(comboBox3.Text=="Yok")
+                toplam = int.Parse((carpan_1 + carpan_2));
+            else 
+                toplam = int.Parse((carpan_1 + carpan_2+ carpan_3));
+
+            direnc_degeri = toplam * carpan;
+
+
+            if (comboBox5.Text == "Yok") tol = "Yok";  
+            else if (comboBox5.Text == "Kahverengi") tol = "%1";  
+            else if (comboBox5.Text == "Kırmızı") tol = "%2"; 
+            else if (comboBox5.Text == "Yeşil") tol = "%0.5"; 
+            else if (comboBox5.Text == "Mavi") tol = "%0.25"; 
+            else if (comboBox5.Text == "Mor") tol = "%0.10";  
+            else if (comboBox5.Text == "Gri") tol = "%0.05";  
+            else if (comboBox5.Text == "Altın") tol = "%5";  
+            else if (comboBox5.Text == "Gümüş") tol = "%10";  
+
+            if (comboBox6.Text == "Yok") sck = "Yok";  
+            else if (comboBox6.Text == "Kahverengi") sck = "100 ppm"; 
+            else if (comboBox6.Text == "Kırmızı") sck = "50 ppm"; 
+            else if (comboBox6.Text == "Turuncu") sck = "15 ppm"; 
+            else if (comboBox6.Text == "Sarı") sck = "25 ppm";
+
+
+            if (radioButton1.Checked == true)
+                textBox1.Text = direnc_degeri.ToString() + " Ω";
+            else if (radioButton2.Checked == true)
+            {
+                direnc_degeri = direnc_degeri / 1000.0;
+                textBox1.Text = direnc_degeri.ToString() + " kΩ";
+            }
+            else
+            {
+                direnc_degeri /= 100000.0;
+                textBox1.Text = direnc_degeri.ToString() + " MΩ";
+            }
+            
+            textBox1.Text +=  "," + tol + "," + sck;
+           
+        }
+
+    }
+}
